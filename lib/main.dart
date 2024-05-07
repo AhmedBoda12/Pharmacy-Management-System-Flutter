ae6501993a508e7093b6a4e4f8a51cdb950549b5
AhmedBoda12 <you@example.com>
2024-05-07 18:54:11 +0300

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class PharmacyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Faith Pharmacy',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
